499bee5b8d60ca199b2ff772428b7811861a6474
LuzuraBS <svendsenluzbright@gmail.com>
2024-10-09 09:11:35 +0200

    new website for travels

---
+++ the-first-test/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    
+    <><div>
+      <nav class="navbar background"></nav>
+      <ul class="nav-list"></ul>
+      <div class="logo"></div>
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstockcake.com%2Fi%2Fglobal-travel-representation_1110128_1124025&psig=AOvVaw0lKZ8diWVhQxwq2VfQ2rcY&ust=1728543034469000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKD709_agIkDFQAAAAAdAAAAABAE" />
+    </div>
+
+    <li>
+       <a href='#weather'> Weather </a>
+    </li>
+
+    <li>
+      <a href='currency'> Currency </a>
+    </li>
+    </>
+
+  )
+}
+
+
+
+export default App
